1d8681b43f7d21fdba6682d583fbc0d0d2a518a9
yogeshv07 <yogesh3207k@gmail.com>
2026-06-06 00:04:15 +0530

    added another model

--- app.py
+++ app.py
@@ -1,46 +1,77 @@
 from flask import Flask, render_template, request
 import google.generativeai as genai
+from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
 load_dotenv()
 
-# Get API key from .env file
-api_key = os.getenv("GEMINI_API_KEY")
+# Gemini
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
-# Configure Gemini
-genai.configure(api_key=api_key)
+gemini_model = genai.GenerativeModel(
+    "gemini-2.5-flash"
+)
 
-# Create model
-model = genai.GenerativeModel("gemini-2.5-flash")
+# OpenRouter
+openrouter_client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY")
+)
 
-# Create Flask app
 app = Flask(__name__)
-
 @app.route("/", methods=["GET", "POST"])
 def home():
 
     answer = ""
+    question = ""
+    selected_model = "Gemini 2.5 Flash"
 
     if request.method == "POST":
 
         question = request.form["question"]
+        selected_model = request.form["model"]
 
         try:
-            response = model.generate_content(question)
-            answer = response.text
+
+            if selected_model == "gemini":
+
+                response = gemini_model.generate_content(question)
+                answer = response.text
+
+                selected_model = "Gemini 2.5 Flash"
+
+            else:
+
+                response = openrouter_client.chat.completions.create(
+                    model=selected_model,
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": question
+                        }
+                    ]
+                )
+
+                answer = response.choices[0].message.content
 
         except Exception as e:
-            answer = f"Error: {str(e)}"
+            answer = str(e)
 
     return render_template(
         "index.html",
         answer=answer,
-        question=question if request.method == "POST" else ""
+        question=question,
+        selected_model=selected_model
     )
 
 if __name__ == "__main__":
-    import os
+
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
